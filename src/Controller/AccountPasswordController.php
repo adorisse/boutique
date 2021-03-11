b42706b2ea856802a3981a839b97dc7ab0c9c697
adorisse <djonnele.mamokosse@gmail.com>
2021-03-11 17:27:40 +0100

    ça marche

--- src/Controller/AccountPasswordController.php
+++ src/Controller/AccountPasswordController.php
@@ -2,17 +2,39 @@
 
 namespace App\Controller;
 
+use App\Form\ChangePasswordType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AccountPasswordController extends AbstractController
 {
     /**
      * @Route("/compte/modifier-mon-mot-de-passe", name="account_password")
+     * @param Request $request
      */
-    public function index(): Response
+    public function index(Request $request, UserPasswordEncoderInterface $encoder): Response
     {
-        return $this->render('account/password.html.twig');
+
+        $user = $this->getUser();
+        $form = $this->createForm(ChangePasswordType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $old_pwd = $form->get('old_password')->getData();
+
+
+
+            if ($encoder->isPasswordValid($user, $old_pwd)) {
+            }
+        }
+
+        return $this->render('account/password.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
---
+++ src/Form/ChangePasswordType.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ChangePasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'disabled' => true,
+                'label' => 'Mon adresse mail'
+            ])
+
+            ->add('firstname', TextType::class, [
+                'disabled' => true,
+                'label' => 'Mon Prénom'
+            ])
+            ->add('lastname', TextType::class, [
+                'disabled' => true,
+                'label' => 'Mon nom'
+            ])
+            ->add('old_password', PasswordType::class, [
+                'label' => 'Mon mot de passe Actuel',
+                'mapped' => false,
+                'attr' => [
+                    'placeholder' => 'veuillez saisir votre mot de passe actuel'
+                ]
+            ])
+
+            ->add('new_password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'invalid_message' => 'Vos mots de passe ne sont pas identique!',
+                'required' => true,
+                'first_options' => ['label' => 'Mon nouveau Mot de Passe'],
+                'second_options' => ['label' => 'Confirmez votre nouveau mot de passe'],
+                'label' => 'Mon nouveau Mot de Passe',
+                'attr' => [
+                    'placeholder' => 'Votre nouveau  Mot de Passe ici'
+                ]
+            ])
+
+            ->add('submit', SubmitType::class, [
+
+                'label' => "Mettre à jour"
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
